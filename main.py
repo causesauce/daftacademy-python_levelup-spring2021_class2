4a04796b701ef5aa2d980c4aa4b517a3b5794646
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-03 23:27:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,18 @@ def create_login_session(response: Response, credentials: HTTPBasicCredentials =
         app.session_token = session_token
         app.composition_to_key += 1
         response.set_cookie(key='session_token', value=session_token)
+    else:
+        response.status_code = 401
+        return response
 
 
 @app.post("/login_token", status_code=201)
 def get_login_token(*, response: Response, session_token: str = Cookie(None)):
     if session_token == app.session_token:
         return {"token": session_token}
+    else:
+        response.status_code = 402
+        return response
 
 
 if __name__ == '__main__':
