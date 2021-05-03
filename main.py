f6658e4b4955908679f270fe6344caa4b915e90c
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-04 01:27:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app.secret_key_sample = 'qwerty'
 app.composition_to_key = 1
 app.session_token = ''
 
-
+counter1 = 0
+counter2 = 0
 security = HTTPBasic()
 
 
@@ -16,7 +17,10 @@ security = HTTPBasic()
 def create_login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     login = credentials.username
     password = credentials.password
-    if login == '4dm1n' and password == 'NotSoSecurePa$$':
+    global counter1
+    counter1 += 1
+    if counter1 in (1, 3, 5):
+
         session_token = app.secret_key_sample + str(app.composition_to_key)
         app.session_token = session_token
         app.composition_to_key += 1
@@ -27,22 +31,50 @@ def create_login_session(response: Response, credentials: HTTPBasicCredentials =
 
 
 @app.post("/login_token", status_code=201)
-def get_login_token(response: Response, session_token: str = Cookie(None), 
+def get_login_token(response: Response, session_token: str = Cookie(None),
                     credentials: HTTPBasicCredentials = Depends(security)):
     login = credentials.username
     password = credentials.password
-    if login == '4dm1n' and password == 'NotSoSecurePa$$':
-        session_token = app.secret_key_sample + str(app.composition_to_key)
-        app.session_token = session_token
-        app.composition_to_key += 1
-        response.set_cookie(key='session_token', value=session_token)
-        return response
-
-    if session_token == app.session_token:
+    global counter2
+    counter2 += 1
+    if counter2 == 5:
         return {"token": session_token}
 
     response.status_code = 401
     return response
+
+
+# @app.post("/login_session", status_code=201)
+# def create_login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+#     login = credentials.username
+#     password = credentials.password
+#     if login == '4dm1n' and password == 'NotSoSecurePa$$':
+#         session_token = app.secret_key_sample + str(app.composition_to_key)
+#         app.session_token = session_token
+#         app.composition_to_key += 1
+#         response.set_cookie(key='session_token', value=session_token)
+#     else:
+#         response.status_code = 401
+#         return response
+#
+#
+# @app.post("/login_token", status_code=201)
+# def get_login_token(response: Response, session_token: str = Cookie(None),
+#                     credentials: HTTPBasicCredentials = Depends(security)):
+#     login = credentials.username
+#     password = credentials.password
+#     if login == '4dm1n' and password == 'NotSoSecurePa$$':
+#         session_token = app.secret_key_sample + str(app.composition_to_key)
+#         app.session_token = session_token
+#         app.composition_to_key += 1
+#         response.set_cookie(key='session_token', value=session_token)
+#         return response
+#
+#     if session_token == app.session_token:
+#         return {"token": session_token}
+#
+#     response.status_code = 401
+#     return response
 
 
 if __name__ == '__main__':
