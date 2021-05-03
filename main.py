d50637317d2c9d0a77e725a2b5672c47962fe21b
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-04 00:41:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def create_login_session(response: Response, credentials: HTTPBasicCredentials =
     login = credentials.username
     password = credentials.password
     if login == '4dm1n' and password == 'NotSoSecurePa$$':
-        session_token = app.secret_key_sample + str(app.composition_to_key)
+        session_token = app.secret_key_sample# + str(app.composition_to_key)
         app.session_token = session_token
         app.composition_to_key += 1
         response.set_cookie(key='session_token', value=session_token)
@@ -30,7 +30,7 @@ def create_login_session(response: Response, credentials: HTTPBasicCredentials =
 def get_login_token(*, response: Response, session_token: str = Cookie(None)):
     if session_token == app.session_token:
         response.status_code = 201
-        session_token = app.secret_key_sample + str(app.composition_to_key)
+        session_token = app.secret_key_sample# + str(app.composition_to_key)
         app.session_token = session_token
         app.composition_to_key += 1
         response.set_cookie(key='session_token', value=session_token)
