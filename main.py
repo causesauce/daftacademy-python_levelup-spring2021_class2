125e1711a63d03c2c8452e9a590b90fa5cd92717
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-04 01:46:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app.secret_key_sample = 'qwerty'
 app.composition_to_key = 1
 app.session_token = ''
 
-
 security = HTTPBasic()
 
 
@@ -26,24 +25,28 @@ def create_login_session(response: Response, credentials: HTTPBasicCredentials =
         return response
 
 
+counter = 0
+
+
 @app.post("/login_token", status_code=201)
 def get_login_token(response: Response, session_token: str = Cookie(None)
                     , credentials: HTTPBasicCredentials = Depends(security)):
     login = credentials.username
     password = credentials.password
-    # if login == '4dm1n' and password == 'NotSoSecurePa$$':
-    #     session_token = app.secret_key_sample + str(app.composition_to_key)
-    #     app.session_token = session_token
-    #     app.composition_to_key += 1
-    #     response.set_cookie(key='session_token', value=session_token)
-    #
-    #     return response
+    if login == '4dm1n' and password == 'NotSoSecurePa$$':
+        session_token = app.secret_key_sample + str(app.composition_to_key)
+        app.session_token = session_token
+        app.composition_to_key += 1
+        response.set_cookie(key='session_token', value=session_token)
+        return {"token": session_token}
 
-    # if session_token == app.session_token:
-    #     return {"token": session_token}
+    if session_token == app.session_token:
+        return {"token": session_token}
 
     response.status_code = 401
-    raise Exception(credentials.username, credentials.password, session_token)
+    global counter
+    counter += 1
+    raise Exception(credentials.username, credentials.password, session_token, counter)
     return response
 
 
