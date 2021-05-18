24e023f9f9025b92926dcf5144558d616072cd20
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-18 16:59:23 +0200

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
 import models
+import schemas
 
 
 def get_shippers(db: Session):
@@ -23,3 +24,12 @@ def get_supplier(db: Session, supplier_id: int):
     return (
         db.query(models.Supplier).filter(models.Supplier.SupplierID == supplier_id).first()
     )
+
+
+def get_products(db, id_sup):
+    a = db.query(models.Product.ProductID, models.Product.ProductName, models.Category)\
+        .join(models.Category, models.Product.CategoryID == models.Category.CategoryID)\
+        .filter(models.Product.SupplierID == id_sup).order_by(models.Product.ProductID.desc()).all()
+
+
+    return a
