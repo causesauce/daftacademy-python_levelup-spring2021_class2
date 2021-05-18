d04d4f4606b6ae01573c90776e7b87fc33c6731b
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-18 15:05:06 +0200

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+
+import models
+
+
+def get_shippers(db: Session):
+    return db.query(models.Shipper).all()
+
+
+def get_shipper(db: Session, shipper_id: int):
+    return (
+        db.query(models.Shipper).filter(models.Shipper.ShipperID == shipper_id).first()
+    )
+
+
+def get_suppliers(db: Session):
+    return (
+        db.query(models.Supplier.SupplierID, models.Supplier.CompanyName).all()
+    )
+
+
+def get_supplier(db: Session, supplier_id: int):
+    return (
+        db.query(models.Supplier).filter(models.Supplier.SupplierID == supplier_id).first()
+    )
---
+++ main.py
@@ -0,0 +1,45 @@
+import uvicorn
+from fastapi import APIRouter, Depends, HTTPException, FastAPI
+from pydantic import PositiveInt
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from db import get_db
+
+app = FastAPI()
+
+router = APIRouter()
+
+
+@router.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
+async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
+    db_shipper = crud.get_shipper(db, shipper_id)
+    if db_shipper is None:
+        raise HTTPException(status_code=404, detail="Shipper not found")
+    return db_shipper
+
+
+@router.get("/suppliers")
+async def get_suppliers(db: Session = Depends(get_db)):
+    db_supps = crud.get_suppliers(db)
+    if db_supps is None:
+        raise HTTPException(status_code=404)
+    return db_supps
+
+
+@router.get("/suppliers/{id}")
+async def get_supplier(id: PositiveInt, db: Session = Depends(get_db)):
+    db_supps = crud.get_supplier(db, id)
+    if db_supps is None:
+        raise HTTPException(status_code=404)
+    return db_supps
+
+
+def main():
+    app.include_router(router, tags=["northwind"])
+    uvicorn.run(app)
+
+
+if __name__ == '__main__':
+    main()
