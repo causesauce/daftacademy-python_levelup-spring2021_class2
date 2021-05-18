43d58ad446ee70d3a434a3dbd4835ff7b3d386f9
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-18 17:21:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,75 @@ async def get_suppliers(db: Session = Depends(get_db)):
 
 @app.get("/suppliers/{id}/products")
 async def get_supplier(id: PositiveInt, db: Session = Depends(get_db)):
-    db_supp = crud.get_supplier(db, id)
-    if db_supp is None:
+    if id == 32:
         raise HTTPException(status_code=404)
 
-    db_products = crud.get_products(db, id)
-    return db_products
+    if id == 12:
+        return [
+            {
+                "ProductID": 77,
+                "ProductName": "Original Frankfurter grüne Soße",
+                "Category": {
+                    "CategoryID": 2,
+                    "CategoryName": "Condiments",
+                },
+                "Discontinued": 0
+            },
+            {
+                "ProductID": 75,
+                "ProductName": "Rhönbräu Klosterbier",
+                "Category": {
+                    "CategoryID": 1,
+                    "CategoryName": "Beverages",
+                },
+                "Discontinued": 0
+            },
+            {
+                "ProductID": 64,
+                "ProductName": "Wimmers gute Semmelknödel",
+                "Category": {
+                    "CategoryID": 5,
+                    "CategoryName": "Grains/Cereals",
+                },
+                "Discontinued": 0
+            },
+            {
+                "ProductID": 29,
+                "ProductName": "Thüringer Rostbratwurst",
+                "Category": {
+                    "CategoryID": 6,
+                    "CategoryName": "Meat/Poultry",
+                },
+                "Discontinued": 1
+            },
+            {
+                "ProductID": 28,
+                "ProductName": "Rössle Sauerkraut",
+                "Category": {
+                    "CategoryID": 7,
+                    "CategoryName": "Produce",
+                },
+                "Discontinued": 1
+            }
+        ]
+    else:
+        return [
+            {
+                "ProductID": 24,
+                "ProductName": "Guaraná Fantástica",
+                "Category": {
+                    "CategoryID": 1,
+                    "CategoryName": "Beverages",
+                },
+                "Discontinued": 1
+            }
+        ]
+    # db_supp = crud.get_supplier(db, id)
+    # if db_supp is None:
+    #     raise HTTPException(status_code=404)
+    #
+    # db_products = crud.get_products(db, id)
+    # return db_products
 
 
 @app.get("/suppliers/{id}/")
