93b44af6e1ce3a2591afebe48d8fe55b21009c23
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-18 19:00:38 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -67,9 +67,12 @@ class UpdateSupplier(BaseModel):
     ContactTitle: Optional[constr(max_length=30)]
     Address: Optional[constr(max_length=60)]
     City: Optional[constr(max_length=15)]
+    Region: Optional[constr(max_length=15)]
     PostalCode: Optional[constr(max_length=10)]
     Country: Optional[constr(max_length=15)]
     Phone: Optional[constr(max_length=24)]
+    Fax: Optional[constr(max_length=24)]
+    HomePage: Optional[str]
 
     class Config:
         orm_mode = True
