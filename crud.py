5eeca64348388381606f9a45370ebf16c070b267
Adrian <67865361+causesauce@users.noreply.github.com>
2021-05-18 17:46:33 +0200

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 import models
@@ -27,9 +28,27 @@ def get_supplier(db: Session, supplier_id: int):
 
 
 def get_products(db, id_sup):
-    a = db.query(models.Product.ProductID, models.Product.ProductName, models.Category, models.Product.Discontinued)\
-        .join(models.Category, models.Product.CategoryID == models.Category.CategoryID)\
+    a = db.query(models.Product.ProductID, models.Product.ProductName, models.Category, models.Product.Discontinued) \
+        .join(models.Category, models.Product.CategoryID == models.Category.CategoryID) \
         .filter(models.Product.SupplierID == id_sup).order_by(models.Product.ProductID.desc()).all()
 
-
     return a
+
+
+def add_supplier(supplier: schemas.Supplier, db: Session):
+    supp = models.Supplier(
+        SupplierID= db.query(func.max(models.Supplier.SupplierID)).one()[0]+1,
+        CompanyName=supplier.CompanyName,
+        ContactName=supplier.ContactName,
+        ContactTitle=supplier.ContactTitle,
+        Address=supplier.Address,
+        City=supplier.City,
+        Region=None,
+        PostalCode=supplier.PostalCode,
+        Country=supplier.Country,
+        Phone=supplier.Phone,
+        Fax=None,
+        HomePage=None
+    )
+    db.add(supp)
+    db.commit()
